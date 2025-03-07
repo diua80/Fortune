d7344de64c3b047269d56bc4cb6ec05253dc3bfd
Iryna Yatsenko <iryna.didova@gmail.com>
2025-03-07 11:00:15 +0200

    game done

--- src/index.js
+++ src/index.js
@@ -8,17 +8,36 @@ const refs = {
 refs.start.addEventListener("click", startGame);
 
 function startGame() {
+
     const promises = [...refs.container.children].map(() => createPromise());
+
     Promise.allSettled(promises).then((items) => {
+
         const isWinner =
             items.every(item => item.status === "fulfilled") ||
             items.every(item => item.status === "rejected");
+        
         items.forEach((item, i) => {
+            
+            refs.container.children[i].classList.remove("emojiSmile", "emojiDevil");
+
             setTimeout(() => {
-                refs.container.children[i].textContent = item.value || item.reason;
-           }, 1000 * (i + 1))
+                if (item.value) {
+                    refs.container.children[i].classList.add("emojiSmile");
+                } else {
+                    refs.container.children[i].classList.add("emojiDevil");
+                }
+            if (i === items.length - 1) {
+                const instance = basicLightbox.create(`
+                    <h1 style="color: yellow; font-size: 72px;">
+                    ${isWinner ? "Winner!" : "Looser!"}
+                    </h1>`);
+            instance.show();
+        }
+                // refs.container.children[i].textContent = item.value || item.reason;                                
+            }, 1000 * (i + 1));
         });
-        console.log(isWinner);
+           
     })
  }
 
